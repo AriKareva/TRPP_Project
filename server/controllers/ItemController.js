8a50d9a9d4a4b73f93a247e3ba8d859106906c87
AriKareva <kareva_a_a@mail.ru>
2024-05-14 00:47:16 +0300

    added routing for user

---
+++ server/controllers/ItemController.js
@@ -0,0 +1,17 @@
+class ItemController{
+
+    async create(req, res){
+
+    }
+
+    async getAll(req, res){
+
+    }
+
+    async getItem(req, res){
+
+    }
+
+}
+
+module.exports = new ItemController()
--- server/index.js
+++ server/index.js
@@ -1,21 +1,31 @@
-require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
 const models = require('./models/models.js')
 const express = require('express')
 const sequelize = require('./db')
+const cors = require('cors')
+const router = require('./routes/index.js')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api', router)
 
-const start = async () =>{
-    try{
+
+// app.get('/', (req, res) => {
+//     res.status(200).send({ message: "МАМА Я КРУТОЙ БЭКЕНДЕР!!!" })
+// })
+
+const start = async () => {
+    try {
         await sequelize.authenticate()
         await sequelize.sync()
-        app.listen(PORT, () => console.log(`server starts on port ${PORT }`))
+        app.listen(PORT, () => console.log(`server starts on port ${PORT}`))
     } catch (e) {
         console.log(e)
     }
 }
-
 
 start()
